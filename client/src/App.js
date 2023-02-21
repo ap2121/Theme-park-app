db9ea3cb88de16b4942b8e7bcb131e308bd4c04b
Arthur Pate <arthurpate42@gmail.com>
2023-02-21 15:09:57 -0800

    Basic routes added to test page router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import {Routes, Route} from 'react-router-dom'
 import Nav from './components/Nav/Nav'
 import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import ParkList from './pages/ParkList/Parklist'
+import RideList from './pages/RideList/RideList'
 import './App.css';
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Nav/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path='/parks-list' element={<ParkList/>}></Route>
+        <Route path='/rides-list' element={<RideList/>}></Route>
       </Routes>
      
     </div>
