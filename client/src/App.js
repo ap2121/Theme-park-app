7eb956935bf5a04deb25fc1e9edfeb80b1ca93ad
Arthur Pate <arthurpate42@gmail.com>
2023-02-21 14:59:29 -0800

    added initial routes for nav and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-
+import {Routes, Route} from 'react-router-dom'
+import Nav from './components/Nav/Nav'
+import Home from './pages/Home/Home'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='app'>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+      </Routes>
      
     </div>
   );
