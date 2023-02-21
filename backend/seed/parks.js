69946d1b2d47a253a0db7482d7d5c4d7ded1b845
Bondarni <118389811+Bondarni@users.noreply.github.com>
2023-02-21 14:32:49 -0500

    created controllers folder, index file, and getAllRides and getAllParks controllers.

--- backend/seed/parks.js
+++ backend/seed/parks.js
@@ -1,10 +1,17 @@
-const db = require('../server')
+const db = require('../db')
 const Park = require('../models/park')
 
 db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
 const main = async () => {
-    const parks = [
-        {image: 'https://i.insider.com/5a94606e3aab0529008b475b?width=700&format=jpeg&auto=webp', name:'Joyland Amusement Park', location: 'Kansas', dateClosed: '2006', description: ''}
-    ]
+  const parks = [
+    {
+      image:
+        'https://i.insider.com/5a94606e3aab0529008b475b?width=700&format=jpeg&auto=webp',
+      name: 'Joyland Amusement Park',
+      location: 'Kansas',
+      dateClosed: '2006',
+      description: ''
+    }
+  ]
 }
