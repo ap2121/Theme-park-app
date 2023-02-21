7b2097b6ef4eaf49c19ab4a3b245c95c781c9f7a
Bondarni <118389811+Bondarni@users.noreply.github.com>
2023-02-21 13:54:58 -0500

    made changes to port call in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const logger = require('morgan')
-
+const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        app.listen(process.env.PORT, ()=> {
-            console.log(`Connected to db and listening on port ${process.env.PORT}`)
-        })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Connected to db and listening on port ${PORT}`)
     })
-        .catch((error) => {
-            console.log(error)
-        })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
